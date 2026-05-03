6a260abcb9828893552a9d5f895cc4442b848ce0
iree26 <oyetiboire@gmail.com>
2026-05-03 20:05:29 +0100

    new

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,6 @@ function App() {
     <Routes>
       <Route path="/" element={<div>Home</div>} />
       <Route path="/login" element={<Login />} />
-      <Route path="/client-signup" element={<div>Client Signup</div>} />
-      <Route path="/artisan-signup" element={<div>Artisan Signup</div>} />
       <Route path="/artisan-dashboard" element={<div>Artisan Dashboard</div>} />
       <Route path="/artisan/:id" element={<div>Artisan Profile</div>} />
       <Route path="/chat/:threadId" element={<div>Chat</div>} />
